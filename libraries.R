ec5f48cb6d7d0615653ad26dc655522a9704dcd2
obibabzz <tanguyrousseau38@gmail.com>
2023-02-03 11:31:27 +0100

    librairies à utiliser

--- libraries.R
+++ libraries.R
@@ -1,1 +1,14 @@
+install.packages(c("Rtools",
+                   "sf",
+                   "tidyverse",
+                   "rmarkdown",
+                   "terra",
+                   "ggplot2",
+                   "gdalUtilities"))
+
 library(tidyverse)
+library(rmarkdown)
+library(terra)
+library(ggplot2)
+library(gdalUtilities)
+library(sf)
